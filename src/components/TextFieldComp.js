2cd4250e07cbf069c13618041d446c3bf11dab3c
binarypsilocybin <rogeriosvaldo@gmail.com>
2023-12-31 15:14:01 +0000

    settings page

---
+++ src/components/TextFieldComp.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { FormControl, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+
+const TextFieldComp = () => {
+  const handleChange = () => {};
+  return (
+    <Box mt={3} width="100%">
+      <FormControl fullWidth size="small">
+        <TextField
+          onChange={handleChange}
+          variant="outlined"
+          label="Amount of Questions"
+          type="number"
+          size="small"
+        ></TextField>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default TextFieldComp;
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,5 +1,27 @@
+import SelectField from "../components/SelectField";
+
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
+import TextFieldComp from "../components/TextFieldComp";
+
 const Settings = () => {
-  return <div>Settings</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <SelectField label="Category" />
+      <SelectField label="Difficulty" />
+      <SelectField label="Type" />
+      <TextFieldComp />
+      <Box mt={3} with="100%">
+        <Button fullWidth variant="contained" type="submit">
+          Get Started
+        </Button>
+      </Box>
+    </form>
+  );
 };
 
 export default Settings;
