a2b9c38eb3814bb0a8ba362923de0fbb7c2599fb
binarypsilocybin <rogeriosvaldo@gmail.com>
2023-12-31 08:11:47 +0000

    styling router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import Settings from "./pages/Settings";
 import Questions from "./pages/Questions";
 import FinalScreen from "./pages/FinalScreen";
 
+import { Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" exact element={<Settings />} />
-        <Route path="/questions" element={<Questions />} />
-        <Route path="/score" element={<FinalScreen />} />
-      </Routes>
+      <Container maxWidth="sm">
+        <Box textAlign="center" mt={5}>
+          <Routes>
+            <Route
+              path="/"
+              exact
+              element={
+                <>
+                  <Typography variant="h2" fontWeight="bold">
+                    Quiz App
+                  </Typography>
+                  <Settings />
+                </>
+              }
+            />
+            <Route path="/questions" element={<Questions />} />
+            <Route path="/score" element={<FinalScreen />} />
+          </Routes>
+        </Box>
+      </Container>
     </Router>
   );
 }
