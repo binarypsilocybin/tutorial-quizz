4f57b4ba554ba12c30133000f7a0f146dd948a6b
binarypsilocybin <rogeriosvaldo@gmail.com>
2024-01-01 14:58:04 +0000

    button questions design

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,5 +1,21 @@
+import { Typography, Box, Button } from "@mui/material";
+
 const Questions = () => {
-  return <div>Questions</div>;
+  return (
+    <Box>
+      <Typography variant="h4">Questions 1</Typography>
+      <Typography mt={5}>This is the question?</Typography>
+      <Box mt={2}>
+        <Button variant="contained">Answer 1</Button>
+      </Box>
+      <Box mt={2}>
+        <Button variant="contained">Answer 2</Button>
+      </Box>
+      <Box mt={5}>
+        <Button>Score: 2/6</Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default Questions;
