0cab7461387f95d501ee352ca84b201042c969a4
binarypsilocybin <rogeriosvaldo@gmail.com>
2024-01-02 20:16:12 +0000

    edited questions

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,7 +1,12 @@
-import { Typography, Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Typography, Button, CircularProgress } from "@mui/material";
 import { Box } from "@mui/system";
 import { useSelector } from "react-redux";
 import useAxios from "../hooks/useAxios";
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
 const Questions = () => {
   const {
@@ -9,33 +14,53 @@ const Questions = () => {
     question_difficulty,
     question_type,
     amount_of_question,
+    score,
   } = useSelector((state) => state);
-  console.log(
-    question_category,
-    question_difficulty,
-    question_type,
-    amount_of_question
-  );
-  let apiUrl = `/api.php?amount=${amount_of_question}`;
 
+  let apiUrl = `/api.php?amount=${amount_of_question}`;
   if (question_category) {
     apiUrl = apiUrl.concat(`&category=${question_category}`);
   }
   if (question_difficulty) {
-    apiUrl = apiUrl.concat(`&difficulty=${question_category}`);
+    apiUrl = apiUrl.concat(`&difficulty=${question_difficulty}`);
   }
   if (question_type) {
-    apiUrl = apiUrl.concat(`&type=${question_category}`);
+    apiUrl = apiUrl.concat(`&type=${question_type}`);
   }
 
   const { response, loading } = useAxios({ url: apiUrl });
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [options, setOptions] = useState([]);
+
+  console.log("response", response);
+
+  useEffect(() => {
+    if (response?.results.length) {
+      const question = response.results[questionIndex];
+      let answers = [...question.incorrect_answers];
+      answers.splice(
+        getRandomInt(question.incorrect_answers.length),
+        0,
+        question.current_answer
+      );
+      setOptions(answers);
+    }
+  }, [response]);
+
+  if (loading) {
+    return (
+      <Box mt={20}>
+        <CircularProgress />
+      </Box>
+    );
+  }
 
   console.log(response);
 
   return (
     <Box>
-      <Typography variant="h4">Questions 1</Typography>
-      <Typography mt={5}>This is the question?</Typography>
+      <Typography variant="h4">Questions 1 {questionIndex + 1}</Typography>
+      <Typography mt={5}>{response.results[questionIndex].question}</Typography>
       <Box mt={2}>
         <Button variant="contained">Answer 1</Button>
       </Box>
